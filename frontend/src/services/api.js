8822ade6ac81bcaf283387d7268137cdea43d2e5
AmitPandey-AP <pandeyamit5969@gmail.com>
2026-05-29 10:41:13 +0530

    latest deployment of backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_URL = 'http://localhost:5000';
+export const BACKEND_URL = 'https://agroportal-1.onrender.com';
 
 const api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
